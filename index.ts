a2611d4c3057067cb9a82e610a70c0c76f925fb9
mryamanbutt333 <161078535+mryamanbutt333@users.noreply.github.com>
2024-04-14 19:05:36 +0500

    complete number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+ 
+import inquirer from "inquirer";
+//1 computer will generate a number
+//2 user input guessed number
+//3 user and computer generated number comparing
+const randomNumber = Math.floor(Math.random()*6+1);
+const answers = await inquirer.prompt([
+    {name: "userGuessedNumb",
+    type: "number",
+    message: "please guess a number"
+ },
+]);
+if(answers.userGuessedNumb===randomNumber){
+    console.log("Congratulation you guess a right number!")
+}else{
+    console.log("moye moye you guessed wrong number")
+}
